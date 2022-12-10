338d8a5d2a7ceae3812a539ec62218d380225763
larryyan <yyan27@dons.usfca.edu>
2022-12-10 14:56:33 -0500

    part 2

--- Triangle.py
+++ Triangle.py
@@ -31,7 +31,7 @@ def classifyTriangle(a,b,c):
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
         
-    if a <= 0 or b <= b or c <= 0:
+    if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
     
     # verify that all 3 inputs are integers  
@@ -43,13 +43,13 @@ def classifyTriangle(a,b,c):
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    if c >= a + b or a >= b + c or b >= a + c:
         return 'NotATriangle'
         
     # now we know that we have a valid triangle 
-    if a == b and b == a:
+    if a == b and b == c:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
+    elif a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a ** 2:
         return 'Right'
     elif (a != b) and  (b != c) and (a != b):
         return 'Scalene'
